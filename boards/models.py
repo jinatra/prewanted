f2d15b9b392f3d9fd8aad7b000fbc2d11aa36a32
Jinatra <jinatra@Jinatraui-MacBookAir.local>
2021-10-21 14:33:41 +0900

    Initial Modeling

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.db                       import models
+from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
+from core.models                     import TimeStampModel
+from users.models import User
+
+class Board(TimeStampModel):
+    title   = models.CharField(max_length=100)
+    content = models.TextField()
+    user    = ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'boards'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from core.models import TimeStampModel
+from django.db   import models
 
-# Create your models here.
+class User(TimeStampModel):
+    email    = models.EmailField(max_length=250, unique=True)
+    password = models.CharField(max_length=1000)
+    nickname = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'users'
