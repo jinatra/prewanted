450182dbace57721b1edfb5a69016ac8ab83083c
Jinatra <jinatra@Jinatraui-MacBookAir.local>
2021-10-22 16:46:27 +0900

    Edited: typo

--- users/regex.py
+++ users/regex.py
@@ -1,4 +1,4 @@
 import re
 
 email_validator = re.compile('^[a-zA-Z\d+-.]+@[a-zA-Z\d+-.]+\.[a-zA-Z]{2,3}$')
-password_validator = re.compule('^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{10,}$')
+password_validator = re.compile('^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{10,}$')
